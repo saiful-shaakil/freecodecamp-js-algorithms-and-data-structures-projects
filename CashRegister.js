07d7bb5b7021523fd76fee75f65d27af9b56a2e0
si-shakil <sishakil850@gmail.com>
2022-06-26 20:20:16 +0600

    Cash Register Project Added

--- CashRegister.js
+++ CashRegister.js
@@ -46,3 +46,62 @@ See below for an example of a cash-in-drawer array:
 Solution
 =============
 */
+
+let currencyUnit = {
+  PENNY: 1,
+  NICKEL: 5,
+  DIME: 10,
+  QUARTER: 25,
+  ONE: 100,
+  FIVE: 500,
+  TEN: 1000,
+  TWENTY: 2000,
+  "ONE HUNDRED": 10000,
+};
+
+function checkCashRegister(price, cash, cid) {
+  let changeSum = cash * 100 - price * 100;
+  let changeSumCheck = changeSum;
+  let change = [];
+  let status = "";
+
+  let cidSum = 0;
+  let filteredCid = cid.filter((ele) => ele[1] !== 0).reverse();
+
+  filteredCid.forEach((ele) => {
+    let curr = ele[0];
+    let currSum = ele[1] * 100;
+    cidSum += currSum;
+    let amount = 0;
+    while (changeSum >= currencyUnit[curr] && currSum > 0) {
+      amount += currencyUnit[curr];
+      changeSum -= currencyUnit[curr];
+      currSum -= currencyUnit[curr];
+    }
+    if (amount !== 0) {
+      change.push([curr, amount / 100]);
+    }
+  });
+  if (changeSum > 0) {
+    status = "INSUFFICIENT_FUNDS";
+    change = [];
+  } else if (changeSum == 0 && changeSumCheck == cidSum) {
+    status = "CLOSED";
+    change = cid;
+  } else {
+    status = "OPEN";
+  }
+  return { status: status, change: change };
+}
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 0.01],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 0],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0],
+]);
